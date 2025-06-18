d70231d70e70a69e99d06a44be27c141242ab0ab
RajasuriyaKarthikeyan <rajasuriyakarthikeyan@gmail.com>
2025-06-18 16:24:05 +0000

    reverse

---
+++ beginer/revese.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    cin >> name;
+    cout << "After Reverse : ";
+    int l = name.size();
+    int i=0, j=l-1;
+    while(i < j){
+        char c = name[i];
+        name[i]= name[j];
+        name[j] = c;
+        i++;
+        j--;
+    }
+    cout <<name << endl;
+    return 0;
+}
